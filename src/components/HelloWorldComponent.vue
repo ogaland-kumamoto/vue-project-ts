016d7df2b64d84efed74e32635c53d4e3547e17b
ogaland-kumamoto <kumamoto.kouki@ogaland.com>
2024-06-01 20:43:01 +0900

    CommpotisionAPIの記述方法で、型チェックされるかの確認

---
+++ src/components/HelloWorldComponent.vue
@@ -0,0 +1,25 @@
+<!-- HelloWorldComponent.vue -->
+<script setup lang="ts">
+
+// 静的型付けの確認
+let msg: number
+msg = 'Hello World!'
+
+
+// 値の型を指定しているが、実際には文字列を返している
+function increment(num: number) {
+//                 ^^^^^^^^型注釈
+  return num + 1;
+}
+
+console.log(increment("999"));
+
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+
+</style>

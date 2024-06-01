d9b918684447d79a6c3f932ad816b1418793ac3e
ogaland-kumamoto <kumamoto.kouki@ogaland.com>
2024-06-01 23:38:38 +0900

    fix

---
+++ src/components/HelloWorldComponentOPTION.vue
@@ -0,0 +1,30 @@
+<!-- HelloWorldComponent.vue -->
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+data() {
+  return {
+    // 型宣言できない
+    msg: 'Hello World!'
+  }
+},
+methods: {
+  increment(num: number) {
+    return num + 1;
+  }
+},
+mounted() {
+
+  console.log(this.increment("999"));
+}
+})
+</script>
+
+<style scoped>
+
+</style>
